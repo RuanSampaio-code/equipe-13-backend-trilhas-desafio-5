2ea430d823229c948c401fcbedb33d4cea7a3d4b
RuanSampaio-code <ruansampaio001@gmail.com>
2025-06-27 23:09:35 -0300

    chore: espaçamento

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -2,8 +2,47 @@ import express from "express";
 import UsersController from "../controllers/userController.js";
 
 const router = express.Router();
-
+/**
+ * @swagger
+ * /users:
+ *   get:
+ *     summary: Lista todos os usuários
+ *     tags: [Usuários]
+ *     responses:
+ *       200:
+ *         description: Lista de usuários retornada com sucesso
+ */
 router.get("/users", UsersController.getUsers);
+
+
+/**
+ * @swagger
+ * /register:
+ *   post:
+ *     summary: Cadastra um novo usuário
+ *     tags: [Usuários]
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               name:
+ *                 type: string
+ *                 example: João Silva
+ *               email:
+ *                 type: string
+ *                 example: joao@email.com
+ *               password:
+ *                 type: string
+ *                 example: minhaSenhaSegura
+ *     responses:
+ *       201:
+ *         description: Usuário criado com sucesso
+ *       400:
+ *         description: Dados inválidos
+ */
 router.post("/register", UsersController.registerUser);
 
 
