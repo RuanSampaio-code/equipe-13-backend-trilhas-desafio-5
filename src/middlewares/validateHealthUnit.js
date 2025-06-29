bb6dbdb516a2714f80be83b2d985fea83c52cb25
RuanSampaio-code <ruansampaio001@gmail.com>
2025-06-29 20:43:37 -0300

    refactor: alterações no schema de dados - middlewares

--- src/middlewares/validateHealthUnit.js
+++ src/middlewares/validateHealthUnit.js
@@ -2,15 +2,16 @@ import { body, validationResult } from 'express-validator';
 
 export const validateFavoriteUnit = [
     // Validação do corpo da requisição
+    body('id').notEmpty().withMessage('ID do local é obrigatório'),
     body('userId').notEmpty().withMessage('ID do usuário é obrigatório'),
     body('name').notEmpty().withMessage('Nome da unidade é obrigatório'),
-    body('address.logradouro').notEmpty().withMessage('Logradouro é obrigatório'),
-    body('address.numero').notEmpty().withMessage('Número é obrigatório'),
-    body('address.bairro').notEmpty().withMessage('Bairro é obrigatório'),
-    body('address.cep').notEmpty().withMessage('CEP é obrigatório'),
-    body('address.estado').notEmpty().withMessage('Estado é obrigatório'),
+    body('address').notEmpty().withMessage('Endereço completo é obrigatório'),
     body('longitude').isFloat().withMessage('Longitude deve ser um número'),
     body('latitude').isFloat().withMessage('Latitude deve ser um número'),
+    body('types').isArray({ min: 1 }).withMessage('Types deve ser um array com pelo menos um item'),
+    body('addedAt').notEmpty().withMessage('Data de adição é obrigatória'),
+
+    // Campos opcionais não precisam de validação obrigatória
 
     // Tratamento dos erros
     (req, res, next) => {
